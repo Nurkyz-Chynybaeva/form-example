4b2c1a0b41b3a4cfda8e68d3b691efaac9590bd8
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-04-30 14:08:03 +0600

    package json

--- src/App.js
+++ src/App.js
@@ -1,122 +1,167 @@
-import { useState } from "react";
+
+
+import { useEffect, useRef, useState } from "react";
+import "./App.css"
+import Input from "./components/Input";
 function App() {
-  function formSubmitCallback(event){
-    const data = {
-      firstName : event.target.form[0].value,
+  const classNames = ['App'];
+  const [submitted, setSubmitted] = useState(false);
+  if (submitted) {
+    classNames.push('submitted');
+  }
+  function submitClickCallback() {
+    setSubmitted(true);
+  }
+  function formSubmitCallback(event) {
+    const data = new FormData(event.target);
+    const user = {
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'),
+      gender: data.get('gender'),
+      firstName: data.get('email'),
+
+      
+    
     }
+    
     event.preventDefault();
   }
   return (
-    <form className="App">
+    <form className={classNames.join(" ")} onSubmit={formSubmitCallback}>
       <div>
         <label for="firstName">First name</label>
-        <input type="text"
+        <Input type="text"
           name="firstName"
-          id="firstName" />
+          id="firstName"
+          pattern="[a-zA-Z]+"
+          validity="Hahaha"
+          required />
+        <span className="error">First name is required.</span>
       </div>
       <div>
         <label for="lastName">Last name</label>
-        <input type="text"
+        <Input type="text"
           name="lastName"
-          id="lastName" />
+          id="lastName"
+          pattern="[a-zA-Z ]+"
+          required />
+        <span className="error">Last name is required.</span>
       </div>
       <div>
         <label for="email">Email</label>
-        <input type="email"
+        <Input type="email"
           name="email"
-          id="email" />
+          id="email"
+          // pattern="[a-zA-Z0-9._-+]+@[a-zA-Z0-9._-]+\.com"
+          required />
+        <span className="error">Valid email is reqiured.</span>
       </div>
       <div>
         <label for="phone">Phone</label>
-        <input type="tel"
+        <Input type="text"
           name="phone"
-          id="phone" />
+          id="phone"
+          pattern="(0|\+996)(777|555|700)[0-9]{6}"
+          required />
+        <span className="error">Valid phone number is required.</span>
       </div>
       <div>
         <label for="dateOfBirth">Date of birth</label>
-        <input type="date"
+        <Input type="date"
           name="dateOfBirth"
-          id="dateOfBirth" />
+          id="dateOfBirth"
+          required />
+        <span className="error">Date of birth is required.</span>
       </div>
       <div>
         <label for="gender">Gender</label>
-        <select name="gender"
-          id="gender">
-          <option>- Select -</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
+        <Input name="gender"
+          required
+          type="select"
+          validity="Hihihi"
+          options={{
+            "": "- Select -",
+            "male": "Male",
+            "female": "Female",
+          }}
+          id="gender" />
+        <span className="error">Gender is required.</span>
       </div>
       <div>
         <label>Education</label>
         <div>
           <label for="school">
-            <input type="radio"
+            <Input type="radio"
               name="education"
               id="school"
-              value="school" /> School
-          </label>
+              value="school"
+              required /> School
+         </label>
         </div>
         <div>
           <label for="university">
-            <input type="radio"
+            <Input type="radio"
               name="education"
               id="university"
-              value="university" /> University
-          </label>
+              value="university"
+              required /> University
+         </label>
         </div>
       </div>
       <div>
         <label>Languages</label>
         <div>
           <label for="english">
-            <input type="checkbox"
+            <Input type="checkbox"
               name="language"
               id="english"
               value="english" /> English
-          </label>
+         </label>
         </div>
         <div>
           <label for="kyrgyz">
-            <input type="checkbox"
+            <Input type="checkbox"
               name="language"
               id="kyrgyz"
               value="kyrgyz" /> Kyrgyz
-          </label>
+         </label>
         </div>
         <div>
           <label for="russian">
-            <input type="checkbox"
+            <Input type="checkbox"
               name="language"
               id="russian"
               value="russian" /> Russian
-          </label>
+         </label>
         </div>
       </div>
       <div>
         <label for="address">Address</label>
-        <input type="text"
+        <Input type="text"
           name="address"
-          id="address" />
+          id="address"
+          required />
+        <span className="error">Address is required.</span>
       </div>
       <div>
         <label for="city">City</label>
-        <input type="text"
+        <Input type="text"
           name="city"
-          id="city" />
+          id="city"
+          required />
+        <span className="error">City is required.</span>
       </div>
       <div>
         <label for="zip">ZIP</label>
-        <input type="number"
-          name="zip"
-          id="zip" />
+        <Input type="text"
+          pattern="72[0-9]{4}"
+          required />
+        <span className="error">ZIP must be in 72xxxx format.</span>
       </div>
       <div>
-        <button type="submit" onClick={formSubmitCallback}>Submit</button>
+        <button onClick={submitClickCallback}>Submit</button>
       </div>
     </form>
   );
 }
 export default App;
-
-
